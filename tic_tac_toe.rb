4f64d3b11fe7c9f67957cd0039d75d6ac45d265a
Nicolas Hermet <n.hermet@gmail.com>
2018-07-17 11:02:13 +0200

    Initialisation du fichier tic_tac_toe.rb

---
+++ tic_tac_toe.rb
@@ -0,0 +1,18 @@
+require 'pry'
+
+# Ceci est le plateau de jeu
+class Board
+end
+
+# Ceci est la gestion de chaque
+# case du tableau de jeu
+class BoardCase
+end
+
+# Ceci est la classe des joueurs
+class Player
+end
+
+# Ceci est la classe du jeu entier
+class Game
+end
