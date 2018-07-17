a36d8c2d9b15f044be52dbb608a7f7cc2dd0af5e
Nicolas Hermet <n.hermet@gmail.com>
2018-07-17 11:28:12 +0200

    methode de remplissage de BoardCase, avec test sur valeur et pré-remplissage

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -7,6 +7,28 @@ end
 # Ceci est la gestion de chaque
 # case du tableau de jeu
 class BoardCase
+  attr_writer :value
+
+  def initialize
+    @value = ''
+  end
+
+  def value(value)
+    value.downcase!
+    if !'xo'.include?(value)
+      while !'xo'.include?(value)
+        puts "Ne pas jouer autre chose que 'x' ou 'o' "
+        print '|> '
+        value = gets.chomp.to_s.downcase
+      end
+    end
+    if @value == ''
+      @value = value 
+    else
+      puts "C'est moi où vous essayez de tricher ?"
+      @value
+    end
+  end
 end
 
 # Ceci est la classe des joueurs
@@ -16,3 +38,5 @@ end
 # Ceci est la classe du jeu entier
 class Game
 end
+
+binding.pry
